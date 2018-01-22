9806461e98ab47d0ebde7fb3412358efd855bd25
IvonReD <ivonne.reyes.dc@gmail.com>
2018-01-22 10:47:55 -0600

    Subiendo Proyecto Foodmap

---
+++ js/data.js
@@ -0,0 +1,65 @@
+var data = [{
+    'name': 'Tacos Mexicanos',
+    'image': '../assets/images/tacos.jpg',
+    'description': 'Todo tipo de Tacos',
+    'address': 'Av. Juarez No.34, Cd. Mexico',
+    'money': '$100',
+    'type': 'Mexicana'
+  },
+  {
+    'name': 'La Garnacha',
+    'image': '../assets/images/mex.jpg',
+    'description': 'Los Mejores Antojos',
+    'address': 'Av. 34 sur S/N, Michoacan',
+    'money': '$150',
+    'type': 'Mexicana'
+  },
+  {
+    'name': 'Venecia',
+    'image': '../assets/images/pasta.jpg',
+    'description': 'La especialidad Las pastas',
+    'address': 'Calle Matamoros No.100, Yucatan',
+    'money': '$300',
+    'type': 'Italiana'
+  },
+  {
+    'name': 'Italian´s',
+    'image': '../assets/images/tiramisu.jpg',
+    'description': 'Postres Italianos',
+    'address': 'Av. Revolucion No.60, Queretaro',
+    'money': '$100',
+    'type': 'Italiana'
+  },
+  {
+    'name': 'Valle del Colca',
+    'image': '../assets/images/peru.jpg',
+    'description': 'La mejor comida del Peru',
+    'address': 'Av. Independencia No.5000, Monterrey',
+    'money': '$400',
+    'type': 'Peruana'
+  },
+  {
+    'name': 'El Refugio',
+    'image': '../assets/images/tacos.jpg',
+    'description': 'Buffet',
+    'address': 'Circuito Nacional No.50, Zacatecas',
+    'money': '$200',
+    'type': 'Peruana'
+  },
+  {
+    'name': 'Sushito',
+    'image': '../assets/images/tacos.jpg',
+    'description': 'Rollos de Sushi',
+    'address': 'Calle Aldama S/N, Puebla',
+    'money': '$400',
+    'type': 'Japonesa'
+  },
+  {
+    'name': 'Fuji',
+    'image': '../assets/images/tacos.jpg',
+    'description': 'Todo tipo de Comida',
+    'address': 'Calle Aldama S/N, Puebla',
+    'money': '$200',
+    'type': 'Japonesa'
+  }
+];
